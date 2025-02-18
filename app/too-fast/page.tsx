b97eca99e42c741d2bbbd1c0788ff66891f6d975
kingk <kingk@DESKTOP-Q0U2A5C>
2025-02-19 03:02:04 +0700

    Add Redis integration, and rate limiting functionality

---
+++ app/too-fast/page.tsx
@@ -0,0 +1,10 @@
+const page = () => {
+  return (
+    <main className='root-container flex min-h-screen flex-col items-start justify-center'>
+      <h1 className='font-bebas-neue text-4xl font-bold text-light-100'>Too Fast</h1>
+      <p className='font-bebas-neue text-lg text-center text-light-400'>You're moving too fast, slow down</p>
+    </main>
+  )
+}
+
+export default page
