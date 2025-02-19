0566abbc547b27f288366044a5c92a1c9cce219b
kingk <kingk@DESKTOP-Q0U2A5C>
2025-02-20 03:24:48 +0700

    Implement onboarding workflow and user activity tracking (using qstash)

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,18 +1,43 @@
-import { auth } from "@/auth"
-import Header from "@/components/Header"
-import { redirect } from "next/navigation"
-import { ReactNode } from "react"
+import { ReactNode } from 'react'
+import Header from '@/components/Header'
+import { auth } from '@/auth'
+import { redirect } from 'next/navigation'
+import { after } from 'next/server'
+import { db } from '@/database/drizzle'
+import { users } from '@/database/schema'
+import { eq } from 'drizzle-orm'
 
-const layout = async ({children} : {children: ReactNode} ) => {
+const layout = async ({ children }: { children: ReactNode }) => {
   const session = await auth()
 
   if (!session) redirect('/sign-in')
-  
+
+
+  // after is a lifecycle hook that runs after the component is rendered
+  after(async () => {
+    if (!session?.user?.id) return
+
+    const user = await db
+      .select()
+      .from(users)
+      .where(eq(users.id, session?.user?.id))
+      .limit(1)
+
+    // If the user has already been active today, return
+    if (user[0].lastActivityDate === new Date().toISOString().slice(0, 10))
+      return
+
+    await db
+      .update(users)
+      .set({ lastActivityDate: new Date().toISOString().slice(0, 10) })
+      .where(eq(users.id, session?.user?.id))
+  })
+
   return (
-    <main className="root-container">
-      <div className="mx-auto max-w-7xl">
+    <main className='root-container'>
+      <div className='mx-auto max-w-7xl'>
         <Header session={session} />
-        <div className="mt-20 pb-20">{children}</div>
+        <div className='mt-20 pb-20'>{children}</div>
       </div>
     </main>
   )
